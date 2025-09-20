8fdda180ee4cc0600071e67a5125befbbdb849b2
DESKTOP-Q5CFOMV\user <sandy424@naver.com>
2025-09-20 10:15:21 +0900

    [update] useEffect

--- beginners/src/App.js
+++ beginners/src/App.js
@@ -1,9 +1,27 @@
-import Button from './components/Button.js';
+import {useEffect, useState} from "react";
 
 function App() {
+  
+  const [key, setKey] = useState("");
+  const [counter, setValue] = useState(0);
+
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKey(event.target.value);
+
+  useEffect(() => {
+    console.log("Call the API....");
+  }, []);
+  useEffect(() => {
+    if(key !== "" && key.length > 5){
+      console.log("Search for", key);
+    }
+  }, [key]);
+
   return (
     <div>
-      
+      <input type="text" placeholder="Search here" onChange={onChange} value={key}/>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
