4b37acd42fe4680a3c92a27741307794dfe8c7e9
DESKTOP-UKB19P3\338 <sandy424@naver.com>
2025-09-23 19:01:20 +0900

    [add] react-router-dom 설치 후 component 나누기

--- beginners/src/App.js
+++ beginners/src/App.js
@@ -1,39 +1,15 @@
-import {useEffect, useState} from "react";
+import {Routes, Route} from "react-router-dom";
+import Main from '../component/Main';
+import Movie_list from '../component/Movie_list';
 
 function App() {
-    const [loading, setLoading] = useState(true);
-    //입력되는 영화 데이터들을 담는 배열
-    const [movies, setMovies] = useState([]);
-    const getMovies = async() => {
-      const json = await (await fetch(
-        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
-      )).json();
-      setMovies(json.data.movies);
-      setTimeout(() => {
-        setLoading(false);
-      }, 2000); // 2초 동안 Loading 유지
-      };
-    useEffect(() => {
-      getMovies();
-    }, []);
-
-  console.log(movies);
-  return (
-    <div>
-      {loading ? <h1>Loading...</h1> :
-      <div>{movies.map((movie) => (
-        <div key={movie.id}>
-          <h2>{movie.title}</h2>
-          <img src={movie.medium_cover_image} alt=""/>
-          <p>{movie.summary}</p>
-          <ul>
-            {movie.genres.map((g) => (
-              <li key={g}>{g}</li>
-            ))}
-          </ul>
-        </div>
-      ))}</div>}
+    return(
+      <div>
+      <Routes>
+        <Route path="/" element={<Main/>}/>
+        <Route path="/Movie_list" element={<Movie_list/>}/>
+      </Routes>
     </div>
-  );
+    );
 }
 export default App;
