96056b9bb9d44ab712498bcc658a02a71a46e2cc
DESKTOP-UKB19P3\338 <sandy424@naver.com>
2025-09-30 17:34:18 +0900

    배포하려고 했는데 막혀서 나중에 css 코드 넣고 완성 시킨 다음에 해보

--- beginners/src/App.js
+++ beginners/src/App.js
@@ -7,7 +7,7 @@ function App() {
     return(
       <div>
       <Routes>
-        <Route path="/" element={<Main/>}/>
+        <Route path={process.env.PUBLIC_URL+"/"} element={<Main/>}/>
         <Route path="/Movie_list" element={<Movie_list/>}/>
         <Route path="/movie/:id" element={<Detail/>}/>
       </Routes>
