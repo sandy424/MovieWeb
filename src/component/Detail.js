37c6308b69c9e53d8ba571fe24393ba1c96100ce
DESKTOP-UKB19P3\338 <sandy424@naver.com>
2025-10-28 19:32:51 +0900

    style 어떻게 하지...

--- src/component/Detail.js
+++ src/component/Detail.js
@@ -7,13 +7,16 @@ function Detail() {
         const json = await(
             await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
         ).json();
+        setMovie(json.data.movie);
     }
     useEffect(() => {
         getMovie();
     }, []);
 
     return(
-        <h1>Detail</h1>
+        <div>
+            
+        </div>
     );
 }
 export default Detail;
--- src/component/Main.js
+++ src/component/Main.js
@@ -1,13 +1,18 @@
 import {useNavigate} from "react-router-dom";
+import "../style/style-main.css"
 
 function Main() {
     const navigate = useNavigate();
     const onClick = () => {
         navigate("/Movie_list")
     }
+    const style = {
+        color: "yellow",
+        fontsize: "98px"
+    }
     return(
         <div>
-            <h2>MoviePickr</h2>
+            <h2 style={style}>MoviePickr</h2>
             <button onClick={onClick}>Let's watch</button>
         </div>
     )
