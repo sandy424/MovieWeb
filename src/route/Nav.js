48978d509b01315730f38c74c3727ed2bc493b9a
DESKTOP-UKB19P3\338 <sandy424@naver.com>
2025-10-29 12:48:16 +0900

    [add] route-Nav 상단바 추가

---
+++ src/route/Nav.js
@@ -0,0 +1,14 @@
+
+
+function Navigate() {
+
+    const style = {
+        color: "yellow",
+    }
+    return(
+        <div>
+            <h2 style={style}>MoviePickr</h2>
+        </div>
+    )
+}
+export default Navigate;
